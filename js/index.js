0391c26eec148da405503775a4f2950f2da20709
AlfredoCastrogiovanni <castro.alfry@gmail.com>
2023-11-27 09:53:28 +0100

    correzione dati img

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 
 const team = [
-    {name: "Wayne Barnett", role: "Founder & CEO", picture: "img/wayne-barnett-founder-ceo.jpg"},
-    {name: "Angela Caroll", role: "Chief Editor", picture: "img/angela-caroll-chief-editor.jpg"},
-    {name: "Walter Gordon", role: "Office Manager", picture: "img/walter-gordon-office-manager.jpg"},
-    {name: "Angela Lopez", role: "Social Media Manager", picture: "img/angela-lopez-social-media-manager.jpg"},
-    {name: "Scott Estrada", role: "Developer", picture: "img/scott-estrada-developer.jpg"},
-    {name: "Barbara Ramos", role: "Graphic Designer", picture: "img/barbara-ramos-graphic-designer.jpg"},
+    {name: "Wayne Barnett", role: "Founder & CEO", picture: "wayne-barnett-founder-ceo.jpg"},
+    {name: "Angela Caroll", role: "Chief Editor", picture: "angela-caroll-chief-editor.jpg"},
+    {name: "Walter Gordon", role: "Office Manager", picture: "walter-gordon-office-manager.jpg"},
+    {name: "Angela Lopez", role: "Social Media Manager", picture: "angela-lopez-social-media-manager.jpg"},
+    {name: "Scott Estrada", role: "Developer", picture: "scott-estrada-developer.jpg"},
+    {name: "Barbara Ramos", role: "Graphic Designer", picture: "barbara-ramos-graphic-designer.jpg"},
 ];
 
 const wrapper = document.querySelector(".cardWrapper");
@@ -21,7 +21,7 @@ function createCard(cardInfo, cardWrapper) {
         <!-- Article ${i} -->
         <article class="card">
             <div class="imgWrapper">
-                <img src="${member.picture}" alt="Picture of ${member.name}">
+                <img src="img/${member.picture}" alt="Picture of ${member.name}">
             </div>
             <div class="infoWrapper">
                 <h2>${member.name}</h2>
